02e3df162be949febd164685cdd076dcc446e804
zhangyanwei <zhangyanwei@zbj.com>
2017-10-09 18:29:15 +0800

    学习express

---
+++ README.md
@@ -0,0 +1,22 @@
+## 项目使用Express生成器生成
+### 安装生成器
+`npm install express-generator -g`   
+
+创建项目目录：
+```
+mkdir express_demo
+cd express_demo
+```
+设置模板引擎：    
+```
+express -h   
+express --hbs
+//选项：选择y
+```
+执行：
+```
+npm install
+//Windows 平台使用如下命令：
+set DEBUG=myapp & npm start
+```
+访问[http://localhost:3000/](http://localhost:3000/)
---
+++ routes/index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/**demo 路由：get */
+router.get('/get', function(req,res) {
+  res.send('GET request to the page');
+})
+
+/**demo 路由：post */
+router.post('/post', function(req,res) {
+  res.send('POST request to the page');
+})
+
+/**app.all() 是一个特殊的路由方法，没有任何 HTTP 方法与其对应，它的作用是对于一个路径上的所有请求加载中间件。 */
+router.all('/secret', function (req, res, next) {
+  console.log('Accessing the secret section ...');
+  next(); // pass control to the next handler
+}, function(req,res) {
+  res.send("next")
+});
+
+/**
+ * 响应方法：
+ * res.downlowd() //提示下载文件
+ * res.end()  //终结响应处理流程
+ * res.json()  //发送一个json格式的响应
+ * res.jsonp()  //发送一个支持JSONP的JSON格式的响应
+ * res.redirece()  //重定向请求
+ * res.render()  //渲染视图模板
+ * res.send()    //发送各种类型的响应
+ * res.sendFile() //以八位字节流的形式发送文件
+ * res.sendStatus() //设置响应状态代码，并将其以字符串形式作为响应体的一部分发送
+ */
+
+module.exports = router;
